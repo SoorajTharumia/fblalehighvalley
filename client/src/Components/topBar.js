7b51954c782e25b58332b2da3eb1ad2e28b29e86
SoorajTharumia <sendsooraj@gmail.com>
2022-06-21 16:36:43 -0400

    implemented the navbar and weather

--- client/src/Components/topBar.js
+++ client/src/Components/topBar.js
@@ -11,7 +11,7 @@ export const TopBar = () => {
   const [weather, setWeather] = useState({});
 
   useEffect(() => {
-    fetch(`${api.base}weather?q=Allentown&units=metric&APPID=${api.key}`)
+    fetch(`${api.base}weather?q=Allentown&units=imperial&APPID=${api.key}`)
       .then((res) => res.json())
       .then((data) => {
         setWeather(data);
@@ -27,22 +27,45 @@ export const TopBar = () => {
 
   return (
     <div>
-      <Navbar className="navigation" expand="lg">
-        <Container>
-          <Navbar.Brand>Lehigh Valley Tourism Bureau</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <NavDropdown
-                title="Weather in the Lehigh Valley!"
-                id="basic-nav-dropdown"
-              >
-                
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      {typeof weather.main != "undefined" ? (
+        <Navbar className="navigation" expand="lg">
+          <Container>
+            <Navbar.Brand className='brand'><em>Lehigh Valley Tourism Bureau</em></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ms-auto">
+                <NavDropdown
+                  title={
+                    <div>
+                      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} />
+                      {Math.floor(weather.main.temp)} &deg;F
+                    </div>}
+                  id="collapsible-nav-dropdown" drop='start'>
+                  <NavDropdown.Item>
+                    <h2>Lehigh Valley Weather</h2>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    Current Temperature: {Math.round(weather.main.temp)} &deg;F
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    Feels Like: {Math.round(weather.main.feels_like)} &deg;F
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    Current Conditions:{" "}
+                    {weather.weather[0].description.charAt(0).toUpperCase() +
+                      weather.weather[0].description.slice(1)}
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    Wind Speed: {weather.wind.speed} mph
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
