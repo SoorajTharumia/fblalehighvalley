1c5baecaee461f2866107b8554ac12689c714de6
SoorajTharumia <sendsooraj@gmail.com>
2022-06-24 13:55:05 -0400

    updated styling

--- client/src/Components/header.js
+++ client/src/Components/header.js
@@ -3,7 +3,7 @@ import "./header.css"
 const Header = () => {
     return (
       <div>
-        <h1 className="header" style={{ color: "#1d3557", fontSize: "30px" }}>
+        <h1 className="header" style={{ color: "#1d3557", fontSize: "25px" }}>
           <em>Explore the Lehigh Valley!</em>
         </h1>
 
@@ -11,20 +11,14 @@ const Header = () => {
           className="description"
           style={{
             color: "black grey",
-            fontSize: "17px",
+            fontSize: "15px",
             textAlign: "justify",
           }}
         >
           The Lehigh Valley is a metropolitan region located in eastern
           Pennsylvania and parts of northwestern New Jersey. Three major cities
           make up the Lehigh Valley, being Allentown, Bethlehem, and Easton.
-          Each city has many attractions and things to see. Allentown, the 3rd
-          largest city in Pennsylvania, is the heart of the Lehigh Valley,
-          housing tons of museums, historic centers, and more! Historic
-          Bethlehem has tons of historical sites open to the public, with some
-          dating back to the Revolutionary War! Can't forget about Easton, which
-          has attractions very popular to the public, with some factories of the
-          brands you love! The area is located in the heart of all the action,
+          Each city has many attractions and things to see, ranging from amusement parks to museums to historical landmarks! The area is located in the heart of all the action,
           with easy access to many major cities of the United States, including
           Philadelphia, New York City, and Washington DC! The region fosters a
           sense of community, and it is a great place for people of all ages to
