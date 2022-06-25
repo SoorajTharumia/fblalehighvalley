dc223a1e8de9e20aa1fc20d9d29fa402a9c9c11e
SoorajTharumia <sendsooraj@gmail.com>
2022-06-25 15:43:15 -0400

    removed spacing for screenshots

--- client/src/posts/foodsCardsList.js
+++ client/src/posts/foodsCardsList.js
@@ -4,7 +4,6 @@ import Button from "react-bootstrap/Button";
 import './posts.css';
   
 export default function CardsList() {
-
   const [foods, setFoods] = useState([]);
 
   useEffect(() => {
@@ -17,35 +16,23 @@ export default function CardsList() {
     console.log(foods)
   });
 
-  {
-    return foods.map(food => {
+  {return foods.map(food => {
       if (food.category === "foods") {
         { var link = "http://maps.google.com/?q=" + food.address; }
         return (
           <Card style={{ width: "22rem" }} className="cardBorder cardContent">
             <Card.Body className="vertical">
               <Card.Title>
-                <h1 className="title">
-                  <a href={food.website} target="_blank" rel="noreferrer">
-                    {food.name}
-                  </a>
-                </h1>
+                <h1 className="title"><a href={food.website} target="_blank" rel="noreferrer">{food.name}</a></h1>
                 <h2>Cuisine Type: {food.cuisine}</h2>
                 <p>Address: {food.address}</p>
                 <p>Price Range (out of 4): {food.price}</p>
               </Card.Title>
               <Button className="btn btn-block btn-success">
-                <a
-                  className="directionsBtn"
-                  href={link}
-                  target="_blank"
-                  rel="noreferrer">
-                  Get Directions
-                </a>
+                <a className="directionsBtn" href={link} target="_blank" rel="noreferrer">Get Directions</a>
               </Button>
             </Card.Body>
           </Card>
-        );
-      }
-    })}
-}
+      );
+    }
+  })}}
